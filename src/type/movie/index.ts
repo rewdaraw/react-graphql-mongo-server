ab6cd3bfba0ba62102345a11c792d3151ceadc31
rewdaraw <rewdaraw@gmail.com>
2021-06-30 12:12:46 +0900

    create MovieType

---
+++ src/type/movie/index.ts
@@ -0,0 +1,10 @@
+import { GraphQLObjectType, GraphQLID, GraphQLString } from "graphql";
+
+export const MovieType = new GraphQLObjectType({
+  name: "Movie",
+  fields: () => ({
+    id: { type: GraphQLID },
+    name: { type: GraphQLString },
+    genre: { type: GraphQLString },
+  }),
+});
