de7fac21ce008ef490c2df88cab3ec943ac4e289
rewdaraw <rewdaraw@gmail.com>
2021-06-30 12:50:48 +0900

    connect mongodb

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+require("dotenv").config();
 const express = require("express");
 const graphqlHTTP = require("express-graphql");
+const mongoose = require("mongoose");
 
 const app = express();
+mongoose.connect(
+  `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.c0nff.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+  console.log("----------------");
+  console.log("DBへ接続しました");
+  console.log("----------------");
+});
 
 app.use("/graphql", () => {
   graphqlHTTP({
@@ -9,5 +19,7 @@ app.use("/graphql", () => {
   });
 });
 app.listen(4000, () => {
-  console.log("listening on port:4000");
+  console.log("-----------------------");
+  console.log("port:4000でnodeが稼働中");
+  console.log("-----------------------");
 });
