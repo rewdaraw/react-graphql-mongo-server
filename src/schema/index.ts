ea79ddbd168b8ff872e5f758fe834a3a83c99784
rewdaraw <rewdaraw@gmail.com>
2021-07-01 13:39:31 +0900

    typo

--- src/schema/index.ts
+++ src/schema/index.ts
@@ -7,7 +7,7 @@ import { GET_MOVIE_BY_ID } from "../query/movie";
 const RootQuery = new GraphQLObjectType({
   name: "RootQuery",
   fields: {
-    getUserById: GET_MOVIE_BY_ID,
+    getMovieById: GET_MOVIE_BY_ID,
     getDirectorById: GET_DIRECTOR_BY_ID,
   },
 });
