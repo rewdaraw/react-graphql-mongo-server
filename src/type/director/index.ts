9bf5958c94c6701087e3b7e1daef99091494663d
rewdaraw <rewdaraw@gmail.com>
2021-07-02 08:43:51 +0900

    getMovieで監督も取得できるようにDirectorTypeにmoviesを追加

--- src/type/director/index.ts
+++ src/type/director/index.ts
@@ -1,15 +1,25 @@
 import {
   GraphQLID,
   GraphQLInt,
+  GraphQLList,
   GraphQLObjectType,
   GraphQLString,
 } from "graphql";
+import { Movie } from "../../model/movie";
+import { MovieType } from "../movie";
 
-export const DirectorType = new GraphQLObjectType({
+export const DirectorType: any = new GraphQLObjectType({
   name: "Director",
   fields: {
     id: { type: GraphQLID },
     name: { type: GraphQLString },
     age: { type: GraphQLInt },
+    movies: {
+      type: new GraphQLList(MovieType),
+      // MEMO: Relationはここにresolve書く
+      resolve(parent: any, args: any) {
+        return Movie.find({ directorId: parent.id });
+      },
+    },
   },
 });
